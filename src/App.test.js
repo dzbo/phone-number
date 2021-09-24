462c0da652c485e165eb607dae9dfb3cc86ff331
Dominik Zborowski <dominik.zborowski@gmail.com>
2021-09-24 02:22:52 +0200

    Fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders phone code component", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.getByText("Phone number");
+  expect(element).toBeInTheDocument();
 });
